5cb13a62fc9e25f4438dd077b68fbbf70e9a2128
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-02-14 14:24:13 +0100

    .

--- epibooks-4/src/components/AddComment.jsx
+++ epibooks-4/src/components/AddComment.jsx
@@ -1,44 +1,57 @@
-import { Component } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Component } from 'react'
+import { Button, Form } from 'react-bootstrap'
 
 class AddComment extends Component {
   state = {
     comment: {
-      comment: "",
+      comment: '',
       rate: 1,
-      elementId: this.props.asin
+      elementId: this.props.asin,
+    },
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.asin !== this.props.asin) {
+      this.setState({
+        comment: {
+          ...this.state.comment,
+          elementId: this.props.asin,
+        },
+      })
     }
-  };
+  }
 
   sendComment = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     try {
-      let response = await fetch("https://striveschool-api.herokuapp.com/api/comments", {
-        method: "POST",
-        body: JSON.stringify(this.state.comment),
-        headers: {
-          "Content-type": "application/json",
-          Authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2VhM2EwNjVmZTk4NDAwMTM0ZDNkNjciLCJpYXQiOjE2NzYyOTQ2NjMsImV4cCI6MTY3NzUwNDI2M30.nght25qzty4zflRvgLp-TZUZZ-wK4dSqnV4PwvdUqhM"
+      let response = await fetch(
+        'https://striveschool-api.herokuapp.com/api/comments',
+        {
+          method: 'POST',
+          body: JSON.stringify(this.state.comment),
+          headers: {
+            'Content-type': 'application/json',
+            Authorization: 'Bearer your-auth-token-goes-here',
+          },
         }
-      });
+      )
       if (response.ok) {
-        alert("Comment was sent!");
+        alert('Comment was sent!')
         this.setState({
           comment: {
-            comment: "",
+            comment: '',
             rate: 1,
-            elementId: this.props.asin
-          }
-        });
+            elementId: this.props.asin,
+          },
+        })
       } else {
-        console.log("error");
-        alert("something went wrong");
+        console.log('error')
+        alert('something went wrong')
       }
     } catch (error) {
-      console.log("error");
+      console.log('error')
     }
-  };
+  }
 
   render() {
     return (
@@ -54,8 +67,8 @@ class AddComment extends Component {
                 this.setState({
                   comment: {
                     ...this.state.comment,
-                    comment: e.target.value
-                  }
+                    comment: e.target.value,
+                  },
                 })
               }
             />
@@ -69,8 +82,8 @@ class AddComment extends Component {
                 this.setState({
                   comment: {
                     ...this.state.comment,
-                    rate: e.target.value
-                  }
+                    rate: e.target.value,
+                  },
                 })
               }
             >
@@ -86,8 +99,8 @@ class AddComment extends Component {
           </Button>
         </Form>
       </div>
-    );
+    )
   }
 }
 
-export default AddComment;
+export default AddComment
